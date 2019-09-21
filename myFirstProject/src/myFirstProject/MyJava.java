df492c81d1774035d5992827b5793dc78172cd86
Afernandez86 <afernandez86@knights.ucf.edu>
2019-09-21 14:02:18 -0400

    These are my new changes

---
+++ myFirstProject/src/myFirstProject/MyJava.java
@@ -0,0 +1,11 @@
+package myFirstProject;
+
+public class MyJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Homie!");
+		System.out.print("This is another change for testing");
+	}
+
+}
